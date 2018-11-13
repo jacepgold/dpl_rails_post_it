04060bcc56806454fccb052f4a913bc1a16936c7
Jace Gold <jacegold@Jaces-MacBook-Pro.local>
2018-11-12 20:32:32 -0700

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'notes/index'
-  get 'notes/show'
-  get 'notes/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'notes#index'
+  resources :notes
 end
